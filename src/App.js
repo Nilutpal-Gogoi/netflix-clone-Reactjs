56d2f57a827165608f100104d7235662c561e2ba
nilutpal <nilutpal.ee@gmail.com>
2020-11-24 16:13:04 +0530

    authentication_1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signin, Signup } from "./pages";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes"
 
 export default function App() {
+  const user = null;
   return (
     <Router>
-      <Route exact path = "/browse">
-        <Browse></Browse>
-      </Route>
-      <Route exact path = "/signin">
-        <Signin></Signin>
-      </Route>
-      <Route exact path = "/signup">
-        <Signup></Signup>
-      </Route>
-      <Route exact path = {ROUTES.HOME}>
-        <Home></Home>
-      </Route>
-      
+      <Switch>
+        <IsUserRedirect 
+          user={user} 
+          loggedInPath = {ROUTES.BROWSE} 
+          path={ROUTES.SIGN_IN}
+          exact 
+        >
+          <Signin></Signin>
+        </IsUserRedirect>
+        <IsUserRedirect 
+          user={user} 
+          loggedInPath = {ROUTES.BROWSE} 
+          path={ROUTES.SIGN_UP}
+          exact 
+        >
+          <Signup></Signup>
+        </IsUserRedirect>
+        <ProtectedRoute 
+          user={user} 
+          path={ROUTES.BROWSE} 
+        >
+          <Browse></Browse>
+        </ProtectedRoute>
+        <IsUserRedirect 
+          user={user} 
+          loggedInPath={ROUTES.BROWSE} 
+          path={ROUTES.HOME}
+          exact
+        >
+          <Home></Home>
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
