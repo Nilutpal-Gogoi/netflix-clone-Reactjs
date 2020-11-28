d3cf183f565c7fd9684c4458f1d0e6d8f7c83cfc
nilutpal <nilutpal.ee@gmail.com>
2020-11-28 11:52:04 +0530

    ProfileAndSpinnerComponent

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowseContainer } from "../containers/browse";
+import { useContent } from "../hooks";
+import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
+  // we need the series and the films
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  
+  // we need slides
+  const slides = selectionFilter({ series, films });
+  // pass it to the browse container
+
   return (
-    <p>Hello from Browse!</p>
+    <BrowseContainer slides={slides}></BrowseContainer>
   )
 }
