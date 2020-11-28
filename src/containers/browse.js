de275260f9b668ed7a411dae44718cffa361ed05
nilutpal <nilutpal.ee@gmail.com>
2020-11-28 16:15:40 +0530

    HeaderComponent1

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -3,6 +3,8 @@ import React, {useState, useContext, useEffect} from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../context/firebase";
 import { Header, Loading } from "../components";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
 
 
 export function BrowseContainer({ slides }) {
@@ -26,9 +28,37 @@ export function BrowseContainer({ slides }) {
         ) : (
           <Loading.ReleaseBody></Loading.ReleaseBody>
         )}
-        <Header src="joker1">
-          <p>Hello</p>
-          </Header>
+
+        <Header src="joker1" dontShowOnSmallViewPort>
+          <Header.Frame>
+            <Header.Group>
+              <Header.Logo to ={ROUTES.HOME} src={logo} alt="Netflix"></Header.Logo>
+              <Header.TextLink>Series</Header.TextLink>
+              <Header.TextLink>Films</Header.TextLink>
+            </Header.Group>
+            <Header.Group>
+              <Header.Profile>
+                <Header.Picture src={user.photoURL}></Header.Picture>
+                <Header.Dropdown>
+                  <Header.Group>
+                    <Header.Picture src={user.photoURL}></Header.Picture>
+                    <Header.TextLink>{user.displayName}</Header.TextLink>
+                  </Header.Group>
+                  <Header.Group>
+                    <Header.TextLink onClick={() => firebase.auth().signOut()}>Sign Out</Header.TextLink>
+                  </Header.Group>
+                </Header.Dropdown>
+              </Header.Profile>
+            </Header.Group>
+          </Header.Frame>
+          <Header.Feature>
+            <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+            <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham City. Arthur wears two masks -- the one he paints 
+            for his day job as a clown, and the guise he projects in a futile attempt to feel like he's part of the world around him.
+            </Header.Text>
+          </Header.Feature>  
+        </Header>
       </React.Fragment>
     ) : (
       <SelectProfileContainer user={user} setProfile={setProfile}></SelectProfileContainer>
